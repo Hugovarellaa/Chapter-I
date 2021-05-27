a74bc36a7bf96a4dae1f4dc8ea2b9bc90143ac51
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-05-27 13:55:09 -0300

    Configurando Ambiente
    Aula 08
    
    Configurando Ambiente de desenvolvimento e produção...
    
    -> cross-env

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 export function App(){
-    throw new Error('Eita Giovana, o forninho caiu!')
-
-
     return (
         <h1> Agora sim aprendi</h1>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin') ;
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 
 module.exports = {
-    mode:'development',
-    devtool: 'eval-source-map',
-    entry: path.resolve(__dirname, 'src' , 'index.jsx'),
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
-        path :path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js' , '.jsx'],
+        extensions: ['.js', '.jsx'],
     },
-    plugins:[
+    plugins: [
         new htmlWebpackPlugin({
-            template: path.join(__dirname, 'public' , 'index.html')
+            template: path.join(__dirname, 'public', 'index.html')
         })
     ],
-    devServer:{
-        contentBase : path.join(__dirname, 'public'),
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
     },
     module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            }
-        ],
+        rules: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: 'babel-loader',
+        }],
     }
 
 }

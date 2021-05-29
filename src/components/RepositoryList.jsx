1cda777547230a7989698649bc1643ffff4f355a
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-05-29 18:01:03 -0300

    Chamadas HTTP
    Aula 02
    
    Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,27 +1,33 @@
 import { RepositoryItem } from "./RepositoryItem";
-
+import { useState, useEffect } from "react"
 import '../styles/repositories.scss';
 
 const repository = {
     name: 'unform',
     description: 'forms in React',
-    link : "https://github.com/unform/unform"
+    link: "https://github.com/unform/unform"
 }
+// 
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
+    
     return (
         <section className="repository-List">
             <h1>Lista de repositórios</h1>
 
-
             <ul>
-
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-           
-
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     );

693f54e7e25f45794b7750699151f1b9eea42076
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-27 20:17:56 -0300

    Delete src directory

--- src/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const route = express.Router();
-
-route.get('/', (req, res) => res.render("index.ejs"));
-route.get('/room', (req, res) => res.render("room.ejs"));
-route.get('/create-pass', (req, res) => res.render("create-pass"));
-
-module.exports = route

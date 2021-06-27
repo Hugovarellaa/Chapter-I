5edd7ce996cd038b550df59dbd0f5c5e1523ea21
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-27 19:41:56 -0300

    Quarta aula
    1:00:00

--- src/route.js
+++ src/route.js
@@ -2,5 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', (req, res) => res.render("index.ejs"));
+route.get('/room', (req, res) => res.render("room.ejs"));
+route.get('/create-pass', (req, res) => res.render("create-pass"));
 
 module.exports = route

f484f3476e76feacfeb8eab94c42dcde25d1f3ac
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-30 15:55:59 -0300

    Quarta Aula
    1: 48:00 das 3: 30:00

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,31 @@
-import { Modal } from './modal';
+import Modal from './modal.js';
 
 const modal = Modal()
+const modalTitles = document.querySelector(".modal h2")
+const modalDescription = document.querySelector(".modal p")
+const modalButton = document.querySelector(".modal button")
 
-cosnt checkButtons = document.querySelectorAll(".actions a.check")
 
-//Abri a modal
-modal.open()
+// Botao Marca como lido 
+const checkButtons = document.querySelectorAll(".action a.check");
+checkButtons.forEach(button => {
+  button.addEventListener("click", handleClick)
+})
+
+
+//Botao excluir
+const deleteButton = document.querySelectorAll(".action a.delete")
+deleteButton.forEach(button => {
+  button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+  const text = check ? "Marca como lida " : "Excluir "
+  console.log(modalButton)
+  modalTitles.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+  check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+  modal.open()
+
+}
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,9 +1,17 @@
-module.exports = function Modal(){
-  function open(){
-    //funcionalidade de atribuir a classe active para a modal
+export default function Modal() {
+  const modalWrapper = document.querySelector(".modal-wrapper")
+
+  const cancelButton = document.querySelector(".button.cancel")
+  cancelButton.addEventListener("click", close)
+
+
+  //funcionalidade de atribuir a classe active para a modal
+  function open() {
+    modalWrapper.classList.add("active")
   }
-  function close(){
-    //funcionalidade de remove a classe active da modal
+  //funcionalidade de remove a classe active da modal
+  function close() {
+    modalWrapper.classList.remove("active")
   }
 
 

522fbfe6407442e2885815865c8ba15c870231e1
Hugo <hugovarellaa@gmail.com>
2022-01-20 12:11:42 -0300

    Criando o QuestionController

--- src/route.js
+++ src/route.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const route = express.Router();
 
+const questionControllers = require("./controllers/QuestionController");
+
 route.get("/", (req, res) => res.render("index"));
 route.get("/room", (req, res) => res.render("room"));
 route.get("/create-pass", (req, res) => res.render("create-pass"));
 
-// route.post("/room/:room/:question/:action", (req, res) => res.render(""){})
+route.post("/room/:room/:question/:action", questionControllers.index);
 
 module.exports = route;
--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const route = require("./route");
 const path = require("path");
 
 const server = express();
-server.use(route);
 server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "views"));
 server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
+server.use(route);
 
 server.listen(3333, () => console.log("Servidor rodando"));

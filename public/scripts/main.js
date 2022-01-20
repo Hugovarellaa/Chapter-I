438c8540e826e36a318f204c1e14ab08a01ca599
Hugo <hugovarellaa@gmail.com>
2022-01-20 11:14:38 -0300

    modal

---
+++ public/scripts/main.js
@@ -0,0 +1,41 @@
+import Modal from "./modal.js";
+
+const modal = Modal();
+const modalTitle = document.querySelector(".modal h2");
+console.log(modalTitle);
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
+
+const checkButton = document.querySelectorAll(".actions a.check");
+
+checkButton.forEach((button) => {
+  //adicionar a escuta
+  button.addEventListener("click", handleClick);
+});
+
+const deleteButton = document.querySelectorAll(".actions a.delete");
+
+deleteButton.forEach((button) => {
+  button.addEventListener("click", (event) => handleClick(event, false));
+});
+
+function handleClick(event, check = true) {
+  event.preventDefault();
+
+  modalTitle.innerHTML = check ? "Marca como lida" : "Excluir pergunta";
+
+  modalButton.innerHTML = check
+    ? "Sim ,marca como lida"
+    : "Sim, excluir pergunta";
+
+  modalDescription.innerHTML = check
+    ? "Tem certeza que deseja marcar como lida esta pergunta"
+    : "Tem certeza que deseja excluir esta pergunta";
+
+  check
+    ? modalButton.classList.remove("red")
+    : modalButton.classList.add("red");
+
+  //abrir modal
+  modal.open();
+}
